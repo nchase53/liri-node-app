863d81d69c8beefb74278dccc11d956edb10bb3d
Nolan <nchase53@yahoo.com>
2017-10-20 23:10:57 -0400

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '<Ealp1Quqh4oa3jBmARlfzzRKL>',
+  consumer_secret: '<zTF1JY9GhS477MpKsgFQocuzuvucTJv8XI3ColUAdzK6URHhBI>',
+  access_token_key: '<921115895694544896-39D6DeMNfzrUmbLyjnZtuiee5vGN6qJ>',
+  access_token_secret: '<YmDxvNr7BQWhN3NKQr7vEe6uzvxRCXKEhEO7Mck7IybHV>',
+}
+
+module.exports = twitterKeys;
